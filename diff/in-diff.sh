a91a24a47ee5d40fc7b124deb56f12784fe5e4e8
mh <observermatth@gmail.com>
2022-07-16 05:09:48 -0300

    [update(bin)] update `bin` scripts to also send some to `side` .NOTE: maybe make this repository public? .add(diff) .add(pkg/zsh-plugins.sh)

---
+++ diff/in-diff.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/sh
+
+case $1 in
+	'-h'|'--help')
+		echo "Usage: in-diff.sh [--new] <infile>"
+		echo "Variables:
+	DIFF : diff-like command"
+		exit 1
+esac
+
+if [[ $# = 0 || $# -ge 3 ]]
+then
+	echo "[ !! ] Bad usage. See in-diff.sh --help" >&2
+	exit
+fi
+
+NEW=false
+if [[ $1 = '--new' ]]
+then
+	NEW=true
+	FILE=$2
+elif [[ ${1:0:2} = '--' ]]
+then
+	echo "[ !! ] Bad usage. See github.sh --help" >&2
+	exit 1
+else
+	FILE=$1
+fi
+
+ORIG=${FILE%.in}
+
+if [[ ${ORIG} == ${FILE} ]]
+then
+  echo "[ !! ] File \`${FILE}' is not an infile"
+  exit 1
+fi
+
+[[ -z $DIFF ]] && DIFF='/usr/bin/diff --color=always -u'
+
+if $NEW
+then
+	$DIFF ${ORIG} ${FILE}
+else
+	$DIFF ${FILE} ${ORIG}
+fi
