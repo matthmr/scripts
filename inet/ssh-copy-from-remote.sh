08ba272f414befcdf2ba62fd591b7db8005301d1
mh <observermatth@gmail.com>
2022-09-05 23:01:11 -0300

    (ssh) made more specific scripts for `scp`

---
+++ inet/ssh-copy-from-remote.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+
+case $1 in
+	'-h'|'--help')
+		echo "Usage:       ssh-copy-from-remote.sh <user> <host> <remote> <local> [ssh options]"
+		echo "Description: Copies a file from a remote ssh server"
+		echo "
+		VARIABLES:
+			SCP: [ssh copy client]"
+		exit 1
+		;;
+esac
+
+[[ -z $SCP ]] && SCP=scp
+
+USER=$1
+HOST=$2
+REMOTE=$3
+LOCAL=$4
+
+$SCP "${@:5}" "$USER@$HOST:$REMOTE" "$LOCAL"
