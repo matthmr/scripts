f23b2933994294046e7133b01bf2f5d118b5f9ef
mh <observermatth@gmail.com>
2022-12-01 21:35:23 -0300

    up(inet/git): major re-write .up(git/gitinclude): new emacs packages .up(sh/functions): add `mkproj`

--- inet/git-remote-push.sh
+++ inet/git-remote-push.sh
@@ -5,13 +5,23 @@ AS=${0##*/}
 case $1 in
   '-h'|'--help')
     if [[ $AS = 'git-remote-push.sh' ]]; then
-      echo "Usage:       git-remote-push.sh <repo> <remote>"
+      echo "Usage:       git-remote-push.sh [-s] <local-repo> <remote-address> [<remote-name>] -- [git options]"
       echo "Description: Pushes a git repository to the standard remote"
+      echo "Options:
+  -s: <local-repo> is relative to the standard source, otherwise it's
+      relative to the current working directory
+  -a <remote-name>: use <remote-name> instead of <local-repo> as the
+                    repo name in <remote-address>"
       echo "VARIABLES:
       GIT: [git client]"
     elif [[ $AS = 'git-remote-pull.sh' ]]; then
-      echo "Usage:       git-remote-pull.sh <repo> <remote>"
+      echo "Usage:       git-remote-pull.sh [-s] <local-repo> <remote-address> [<remote-name>] -- [git options]"
       echo "Description: Pulls a git repository from the standard remote"
+      echo "Options:
+  -s: <local-repo> is relative to the standard source, otherwise it's
+      relative to the current working directory
+  -a <remote-name>: use <remote-name> instead of <local-repo> as the
+                    repo name in <remote-address>"
       echo "VARIABLES:
       GIT: [git client]"
     fi
@@ -20,22 +30,63 @@ esac
 
 [[ -z $GIT ]] && GIT=git
 
-BASE=/home/mh/Git/MH
+BASE=
+REPO=
+REMOTE=
+REMOTE_REPO=
+GIT_OPT=
 
-REPO=$1
-REMOTE=$2
+lock_repo=false lock_git=false
+has_repo=false has_remote=false
 
-if [[ -z $REPO || -z $REMOTE ]]; then
-  echo "[ !! ] Wrong usage. See --help"
+for arg in $@; do
+  if $lock_git; then
+    GIT_OPT+=$arg
+    continue
+  fi
+
+  if [[ $arg = '--' ]]; then
+    lock_git=true
+    continue
+  elif [[ $arg = '-s' ]]; then
+    lock_repo=true
+    BASE=/home/mh/Git/MH/
+    continue
+  fi
+
+  if $lock_repo; then
+    lock_repo=false
+    has_repo=true
+    has_std=true
+    REPO=$arg
+  else
+    if $has_repo; then
+      if $has_remote; then
+        REMOTE_REPO=$arg
+      else
+        REMOTE=$arg
+        has_remote=true
+      fi
+    else
+      REPO=$arg
+      has_repo=true
+    fi
+  fi
+done
+
+if [[ $lock_repo = true ]] || [[ $has_repo = false ]]; then
+  echo "[ !! ] Missing repo name"
   exit 1
 fi
 
-if [[ ! -d $BASE/$REPO ]]; then
-  echo "[ !! ] Repo \`$REPO' is not a standard repo"
-  exit 1
+if [[ -z $REMOTE_REPO ]]; then
+  REMOTE_REPO=$REPO
 fi
 
-if [[ ! $REMOTE =~ [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
+if [[ -z $REMOTE ]]; then
+  echo "[ !! ] Missing remote"
+  exit 1
+elif [[ ! $REMOTE =~ [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
   echo "[ !! ] Remote \`$REMOTE' is not an ip address"
   exit 1
 fi
@@ -48,5 +99,5 @@ case $AS in
     CMD=push;;
 esac
 
-echo "[ == ] Calling as: $GIT -C $BASE/$REPO $CMD git://$REMOTE/$REPO"
-$GIT -C $BASE/$REPO $CMD git://$REMOTE/$REPO
+echo "[ == ] Calling as: $GIT $GIT_OPT -C $BASE$REPO $CMD git://$REMOTE/$REMOTE_REPO"
+$GIT $GIT_OPT -C $BASE$REPO $CMD git://$REMOTE/$REMOTE_REPO
