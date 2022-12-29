ec8691c727347b55bd3561056cadb0c6e32a5fb9
mh <observermatth@gmail.com>
2022-12-29 09:13:46 -0300

    add(diff/diff.sh): generic diff script
    add(find/jap-dict.sh): find words in the generic japanese dictionary
    up(left-toggle): actually use `ACC`; add help message
    up(sh/alias): remove `figlet`
    up(x/xsession): set cursor by default

---
+++ diff/diff.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/bash
+
+case $1 in
+  '-h'|'--help')
+    echo "Usage:       diff.sh [-] [file] [+] [file] -- [diff-opts]"
+    echo "Description: Verbose diff"
+    echo "Options:
+  -: take [file] as the file with the missing lines
+  +: take [file] as the file with the added lines"
+    echo "Variables:
+  DIFF=\`diff'-like command"
+    exit 1;;
+esac
+
+[[ -z $DIFF ]] && DIFF='diff --color=always -u'
+
+function die {
+  echo "[ !! ] Bad usage. See --help"
+  exit 1
+}
+
+has_minus=false
+has_plus=false
+escape=false
+
+lock_minus=false
+lock_plus=false
+
+DIFF_OPT=""
+MINUS_FILE=""
+PLUS_FILE=""
+
+for arg in $@; do
+  if $escape; then
+    DIFF_OPT+=" $arg"
+    continue
+  elif $lock_minus; then
+    lock_minus=false
+    if $has_minus; then
+      die
+    fi
+    has_minus=true
+    MINUS_FILE="$arg"
+    continue
+  elif $lock_plus; then
+    lock_plus=false
+    if $has_plus; then
+      die
+    fi
+    has_plus=true
+    PLUS_FILE="$arg"
+    continue
+  fi
+
+  case $arg in
+    '-')
+      lock_minus=true;;
+    '+')
+      lock_plus=true;;
+    '--')
+      escape=true;;
+    *)
+      if ! $has_minus; then
+        has_minus=true
+        MINUS_FILE="$arg"
+      elif ! $has_plus; then
+        has_plus=true
+        PLUS_FILE="$arg"
+      else
+        die
+      fi
+  esac
+done
+
+if [[ -z $MINUS_FILE ]] || [[ -z $PLUS_FILE ]]; then
+  die
+fi
+
+echo "[ == ] Running as: $DIFF $DIFF_OPT $MINUS_FILE $PLUS_FILE"
+$DIFF $DIFF_OPT $MINUS_FILE $PLUS_FILE
---
+++ find/jap-dict.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+case $1 in
+	'-h'|'--help')
+		echo "Usage:       jap-dict.sh -jr [word]"
+		echo "Description: Searches the default japanese dictionary for [word"
+    echo "Options:
+  DEFAULT: -j
+
+  -j: search in japanese
+  -r: search in romaji
+  -e: search in english"
+    echo "Variables:
+  GREP=\`grep'-like command
+  JAPDICT=japanese dictionary path"
+		exit 1;;
+esac
+
+[[ -z $JAPDICT ]] && JAPDICT=/home/mh/Documents/Dict/edict-utf8
+[[ -z $GREP ]]    && GREP=grep
+
+case $1 in
+  '-j'|'-e')
+    $GREP --color=auto -F "${@:2}" $JAPDICT;;
+  '-r')
+    $GREP --color=auto -F "$(romaji ${@:2})" $JAPDICT;;
+  *)
+    $GREP --color=auto -F "${@:1}" $JAPDICT;;
+esac
