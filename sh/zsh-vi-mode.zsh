cee208d01098f62141944ea1e2ef3ab0fc8fa614
mh <observermatth@gmail.com>
2023-03-18 06:57:24 -0300

    up(sh/zsh-vi-mode.zsh): remove the change of cursor style when changing modes;
      always using block cursor
    
    up(sh/zsh_environment): using `emacsc` as default editor via environment
      variable (overridden by configs)
    up(sh/zshrc): using `HIST_IGNORE_SPACE` as `setopt` to ignore caching commands
      prefixed with space in history

--- sh/zsh-vi-mode.zsh
+++ sh/zsh-vi-mode.zsh
@@ -14,30 +14,7 @@ bindkey -M vicmd '?' history-incremental-search-forward
 bindkey -M vicmd '/' history-incremental-search-backward
 
 bindkey -v '^?' backward-delete-char
-
-# Change cursor shape for different vi modes.
-function zle-keymap-select {
-  if [[ ${KEYMAP} == vicmd ]] ||
-     [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
-  elif [[ ${KEYMAP} == main ]] ||
-       [[ ${KEYMAP} == viins ]] ||
-       [[ ${KEYMAP} = '' ]] ||
-       [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
-  fi
-}
-
-zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
-
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
-postexec() { echo -ne '\e[1 q' ;} # Use beam shape cursor for each new prompt.
+echo -ne '\e[1 q' # Use beam shape cursor on startup.
 
 # autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
