e3004381b5d7b11094596e7010b4f41a3a8b6f38
mh <observermatth@gmail.com>
2023-03-23 16:40:45 -0300

    bump(net/youtube-json.sh): v1.0.0 -> v2.3.0
    rm(bin/ytres): duplicate

--- inet/youtube-json.sh
+++ inet/youtube-json.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # Integrated by mH (https://github.com/matthmr)
 
@@ -8,206 +8,199 @@
 
 # youtube-json.sh [-c:clear query,-l:list queries] <query>
 
+# TODO: also display the channel id/handle
 
 YOUTUBE_BASE="https://www.youtube.com"
 YOUTUBE_QUERY="$YOUTUBE_BASE/results?search_query="
 YOUTUBE_WATCH="$YOUTUBE_BASE/watch?v="
-AWK_BASE=/home/mh/Scripts/inet #change this
-YTDOWNLOAD=yt-dlp #change this
 BASE=/tmp/youtube
 
-function encode {
-	echo "$@" | tr ' ' '+'
-}
-
-static=n
-JQUERY=".[].videoRenderer | \
-select(. != null) | \
-.title.runs[0].text, .lengthText.simpleText, \
-.ownerText.runs[0].text, .publishedTimeText.simpleText, .shortViewCountText.simpleText, \
-.videoId"
 case $1 in
-	'-s')
-		JQUERY=".[].videoRenderer | \
-select(. != null) | \
-.videoId, \
-.title.runs[0].text, .lengthText.simpleText, \
-.ownerText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url, \
-.ownerText.runs[0].text, .publishedTimeText.simpleText, .shortViewCountText.simpleText, \
-.thumbnail.thumbnails[0].url, \
-.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url"
-		static=y;;
-	'-c')
-		if [[ -d $BASE ]]
-		then
-
-			if [[ $# = 2 ]]
-			then
-				_query="$BASE/$(encode $2)"
-				if [[ -d "$_query" ]]
-				then
-					echo "[ .. ] Removing \`$_query'..." 1>&2
-					rm -rfv "$_query"
-					exit 0
-				else
-					echo "[ !! ] No such query \`$_query' was found" 1>&2
-					exit 1
-				fi
-			fi
-
-			rm -rfv $BASE
-			exit 0
-		else
-			echo "[ !! ] No base was found" 1>&2
-			exit 1
-		fi;;
-	'-l')
-		if [[ -d $BASE ]]
-		then
-			find $BASE -type d | sed 1d | sed 's/\/tmp\/youtube\// -> /g'
-			exit 1
-		else
-			echo "[ !! ] No base was found" 1>&2
-			exit 1
-		fi;;
-	'-h'|'--help')
-		echo "Usage:       ytres [-c:clear <query>] [-s:static webpage] \"<query>\""
-		echo "Description: Generates a static, w3m-viewable webpage with the youtube results of <query> or a simple formated output"
+  '-h'|'--help')
+    echo "Usage:       ytres \"<query>\" [options]"
+    echo "Description: Generates results for an youtube <query>"
+    echo "Options:
+  -l : list queries
+  -w : operation: watch (default)
+  -s : operation: show
+  -c : clear query
+  -C : clear all"
 		echo "Variables:
-	JQ : jq-like command
-	CURL : curl-like command
-	GREP : grep-like command
-	SED : sed-like command"
-		exit 1;;
-	'-v'|'--version')
-		echo "ytres v1.0.0"
-		exit 1;;
+  JQ       : jq-like command
+  CURL     : curl-like command
+  GREP     : grep-like command
+  SED      : sed-like command
+  AWK      : awk-like command
+  FZF      : fzf-like command
+  MPV      : mpv-like command
+  MPV_OPTS : extra mpv options"
+    exit 1;;
+  '-v'|'--version')
+    echo "ytres v2.3.0"
+    exit 1;;
 esac
 
 if [[ $# -le 0 ]]
 then
-	echo "[ !! ] Bad usage. See ytres --help" >&2 1>&2
-	exit 1
+  echo "[ !! ] Bad usage. See ytres --help" >&2 1>&2
+  exit 1
 fi
 
-[[ -z $CURL ]] && CURL=/bin/curl
-[[ -z $GREP ]] && GREP=/bin/grep
-[[ -z $SED ]] &&  SED=/bin/sed
-[[ -z $JQ ]] &&   JQ=jq
+[[ -z $MPV_OPTS ]] && MPV_OPTS="--ytdl-format=best \
+                                --audio=auto --video=auto"
+[[ -z $CURL ]] && CURL=curl
+[[ -z $GREP ]] && GREP=grep
+[[ -z $SED ]]  && SED=sed
+[[ -z $AWK ]]  && AWK=awk
+[[ -z $MPV ]]  && MPV=mpv
+[[ -z $FZF ]]  && FZF=fzf
+[[ -z $JQ ]]   && JQ=jq
 
-echo "[ .. ] Setting up the static website" 1>&2
-if [[ $static = 'n' ]]
-then
-	QUERY=$(encode "$1")
-else
-	QUERY=$(encode "$2")
+OP='download'
+JQUERY=".contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents[] | \
+.videoRenderer | \
+select(. != null) | \
+.title.runs[0].text, \
+.videoId, \
+.publishedTimeText.simpleText, \
+.lengthText.simpleText,
+.viewCountText.simpleText, \
+.ownerText.runs[0].text, \
+.ownerText.runs[0].navigationEndpoint.browseEndpoint.browseId, \
+.ownerText.runs[0].navigationEndpoint.browseEndpoint.canonicalBaseUrl"
+
+function ytres_encode {
+  echo "$@" | tr ' ' '+'
+}
+
+function ytres_load_video {
+  echo "[ == ] Running: $MPV $MPV_OPTS $1"
+  $MPV $MPV_OPTS "$1"
+}
+
+function ytres_search {
+  local res=$(
+    $AWK -F"\x00" '{printf " %s [%s] [%s] [%s] [%s]\n", $1, $3, $4, $5, $6}' $1 \
+      | cat -n | $FZF)
+
+  if [[ -z $res ]]; then
+    echo "[ !! ] Ignoring" 1>&2
+    exit 1
+  fi
+
+  local ln=$(echo "$res" | $AWK '{print $1}')
+
+  case $OP in
+    'watch')
+      local link=$($AWK -F"\x00" "NR == $ln {print \$2}" $1)
+      ytres_load_video "${YOUTUBE_WATCH}${link}";;
+    'show')
+      $AWK -F"\x00" \
+           "NR == $ln {
+printf \" -- %s -- \nlink: ${YOUTUBE_WATCH}%s\nuploaded: %s\nduration: %s\n\
+views: %s\nby: %s\nid: https://www.youtube.com/channel/%s\nhandle: https://www.youtube.com%s\n\", \
+ \$1, \$2, \$3, \$4, \$5, \$6, \$7, \$8}" $1;;
+  esac
+}
+
+for arg in "$@"; do
+  case $arg in
+    '-w')
+      OP=watch;;
+    '-s')
+      OP=show;;
+    '-c')
+      if [[ -d $BASE ]]
+      then
+        if [[ -d "$BASE/$ID" ]]
+        then
+          echo "[ .. ] Removing $ID..." 1>&2
+          rm -rfv "$BASE/$ID"
+          exit 0
+        else
+          echo "[ !! ] No such id $ID was found" 1>&2
+          exit 1
+        fi
+      else
+        echo "[ !! ] No base was found" 1>&2
+        exit 1
+      fi;;
+    '-C')
+      if [[ -d $BASE ]]
+      then
+        rm -rfv $BASE
+        exit 0
+      else
+        echo "[ !! ] No base was found" 1>&2
+        exit 1
+      fi;;
+    '-l')
+      if [[ -d $BASE ]]
+      then
+        find $BASE -type d | sed -n '2,$s:/tmp/youtube/: -> :p'
+        exit 1
+      else
+        echo "[ !! ] No base was found" 1>&2
+        exit 1
+      fi;;
+    *)
+      QUERY=$arg;;
+  esac
+done
+
+if [[ ! -d $BASE ]]; then
+  TMP=$(mktemp -d "/tmp/youtube.XXX")
+  mv -v $TMP $BASE
 fi
 
-if [[ -d $BASE/$QUERY ]]
-then
-	BASE=$BASE/$QUERY
-	echo "[ !! ] The query \`$QUERY' was already searched" 1>&2
-
-	if [[ $static = 'y' ]]
-	then
-		exit 1
-	fi
-
-	if [[ -f $BASE/query.txt ]]
-	then
-
-		if [[ -f $BASE/index.html ]] # then convert
-		then
-			if [[ -f $BASE/_query.txt ]] # already converted
-			then
-				echo "[ !! ] Previous conversion found" 1>&2
-				cat $BASE/_query.txt
-				exit 0
-			else
-				echo "[ !! ] The query \`$QUERY' was statically generated. Converting it back..." 1>&2
-				$SED -E 's/\/(c|channel|user)\/.*//g' $BASE/query.txt |\
-					$AWK_BASE/youtube-json-convert.awk > $BASE/_query.txt
-				cat $BASE/_query.txt
-				exit 0
-			fi
-		fi
-
-		echo "[ .. ] Outputing results" 1>&2
-		$AWK_BASE/youtube-json.awk \
-			-vBASE="$BASE" -vstatic=$static \
-			$BASE/query.txt
-	else
-		echo "[ !! ] No \`query.txt' file was found for \`$BASE/$QUERY'. youtube-json is probably broken, it is best to run \`youtube-json.sh -c'" 1>&2
-		exit 1
-	fi
-
-	exit 0
+if [[ -z $QUERY ]]; then
+  echo "[ !! ] Missing query" 1>&2
+  exit 1
 fi
-
-if [[ ! -d $BASE ]]
-then
-	TMP=$(mktemp -d "/tmp/youtube.XXX")
-	mv -v $TMP $BASE
-fi
-
-TMP=$(mktemp -d "/tmp/youtube/youtube.XXX")
-mv -v $TMP $BASE/$QUERY
+QUERY=$(ytres_encode $QUERY)
+YOUTUBE_QUERY+="$QUERY"
 BASE=$BASE/$QUERY
 
-echo "[ INFO ] query: \`$QUERY'" 1>&2
-PAGE="$YOUTUBE_QUERY$(encode "$QUERY")"
-echo "[ INFO ] downlading page: \`$PAGE'" 1>&2
-echo "[ .. ] Downloading webpage" 1>&2
-$CURL -s "$YOUTUBE_QUERY/$(encode "$QUERY")" \
-	> $BASE/results.html
-
-echo "[ .. ] Carving up the embedded JSON file" 1>&2
-$GREP -Po \
-	'ytInitialData = {.*};</script>' \
-	$BASE/results.html |\
-	$SED -E 's/^ytInitialData = |;<\/script>$//g' > \
-		$BASE/results.json
-
-echo "[ .. ] Querying JSON for contents" 1>&2
-$JQ -cM \
-	'.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents' \
-	$BASE/results.json > $BASE/contents.json
-
-echo "[ .. ] Querying JSON for video info" 1>&2
-QUERY=$($JQ -cM "$JQUERY" $BASE/contents.json | tr -d '"')
-echo "$QUERY" | sed -E 's/https?:.*//g' > $BASE/query.txt
-
-if [[ $static = 'y' ]]
-then
-	echo "$QUERY" | grep -Eo 'https?:.*' > $BASE/links.txt
-
-	echo "[ .. ] Dowloading assets" 1>&2
-	m=0
-	n=1
-	while read link
-	do
-		if [[ $m -le 0 ]]
-		then
-			echo "[ .. ] Downloading video thumbnail $n/20" 1>&2
-			$CURL -s "$link" > $BASE/"t$n".jpg
-			m=1
-		else
-			echo "[ .. ] Downloading channel icon $n/20" 1>&2
-			$CURL -s "$link" > $BASE/"c$n".jpg
-			m=0
-			n=$((n+1))
-		fi
-	done < $BASE/links.txt
-
-	echo "[ .. ] Making the HTML page" 1>&2
-	$AWK_BASE/youtube-json.awk \
-		-vBASE="$BASE" -vstatic=$static \
-		$BASE/query.txt > $BASE/index.html
+if [[ ! -d $BASE ]]; then
+  TMP=$(mktemp -d "/tmp/youtube/youtube.XXX")
+  mv -v $TMP $BASE
 else
-	echo "[ .. ] Outputing results" 1>&2
-	$AWK_BASE/youtube-json.awk \
-		-vBASE="$BASE" -vstatic=$static \
-		$BASE/query.txt
+  echo "[ !! ] Query was already searched" 1>&2
+  ytres_search $BASE/contents.zsv
+  exit $?
 fi
 
-echo "[ OK ] Done!" 1>&2
+echo "[ .. ] Downloading webpage" 1>&2
+echo "[ == ] Running: $CURL -Ls $YOUTUBE_QUERY > $BASE/results.html" 1>&2
+$CURL -Ls "$YOUTUBE_QUERY" > $BASE/results.html
+
+echo "[ .. ] Carving up the embedded JSON file" 1>&2
+echo "[ == ] Running: $GREP  -o 'ytInitialData = {.*};</script>' $BASE/results.html |\
+	$SED -e 's:ytInitialData = ::' \
+       -e 's:;</script>::' > $BASE/results.json" 1>&2
+$GREP  -o 'ytInitialData = {.*};</script>' $BASE/results.html |\
+	$SED -e 's:ytInitialData = ::' \
+       -e 's:;</script>::' > $BASE/results.json
+
+echo "[ .. ] Querying JSON for contents" 1>&2
+echo "[ == ] Running: $JQ [query] $BASE/results.json | sed | awk > $BASE/contents.zsv"
+$JQ "$JQUERY" $BASE/results.json |\
+  $SED -e 's:^"::' -e 's:"$::'   |\
+  $AWK '
+BEGIN {
+  count = 0;
+}
+{
+  ++count;
+
+  if (count == 8) {
+    count = 0;
+    print;
+  }
+  else {
+    printf("%s\x00", $0);
+  }
+}' > $BASE/contents.zsv
+
+ytres_search $BASE/contents.zsv
+exit $?
