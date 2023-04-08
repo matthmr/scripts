ebf7ed0116dc9c83ea9b9d07a8bc05407aaa568f
mh <observermatth@gmail.com>
2023-04-08 07:46:10 -0300

    fix(x/wmpick): `ans` was not dollar-quoted, resulting in a wrong assertion in
      `header_yesno` and a false negative on starting an xsession
    
    up(x/xsession): minor changes
    fix(git/update-big-repo.sh): rogue commands from the refactor of `685c4933`
    up(sh/alias): `gip` as `git -P` (no pager)

--- git/update-big-repo.sh
+++ git/update-big-repo.sh
@@ -9,14 +9,10 @@ esac
 
 BIG_SRC=/home/mh/Scripts/git/big.txt
 
-git -C "$1" pull origin --depth=1 --rebase -X theirs
-
 while read repo; do
   if [[ $repo =~ ^#.*$ || $repo =~ ^( \t)*$ ]]; then
     continue
   fi
-
-  CMD=$(git_cmd $repo)
 
   echo "[ == ] Running as: git -C $repo pull origin --depth=1 --rebase -X theirs"
   git -C $repo pull origin --depth=1 --rebase -X theirs
