e4f7b5069923e73004ea41299412ddb1b6ff19ee
mh <observermatth@gmail.com>
2023-11-04 16:04:29 -0300

    up(alsa/headset): also mute/unmute `Front` when unmuting/muting `Headset`
    fix(sh/functions): fix a 'bug' that didn't allow shell functions to properly
      pass arguments to their underlying commands
    up(git/git-merge-these.sh): add `-p` option for xargs-hookable output
    up(.gitignore): move along...

--- git/git-merge-these.sh
+++ git/git-merge-these.sh
@@ -2,15 +2,22 @@
 
 set -e
 
+plain=false
+
 case $1 in
   '--help'|'-h')
-    echo "Usage:       git-merge-these.sh <MERGER-BASE> [MERGER-DIFF]"
+    echo "Usage:       git-merge-these.sh [OPTIONS] <MERGER-BASE> [MERGER-DIFF]"
     echo "Description: Returns a list of files which need to be checked \
 given the merger-base, and optionally its diff (generally in the form of the \
 stash or a tag). Without MERGER-DIFF, tries to generate given the previous \
-commit, unless it doesn't exist, in which case it'll panic lmao, The merge \
+commit, unless it doesn't exist, in which case it'll panic lmao. The merge \
 occurs with the current HEAD as MERGER-ONTO"
+    echo "Options:
+  -p: return plain list"
     exit 0;;
+  '-p')
+    plain=true
+    MERGER_BASE=$2;;
   *)
     MERGER_BASE=$1;;
 esac
@@ -20,18 +27,35 @@ if [[ -z $MERGER_BASE ]]; then
   exit 1
 fi
 
-if [[ -z $2 ]]; then
-  echo "[ WW ] Using previous commit as base, MERGER-BASE as MERGER-DIFF" 1>&2
-  MERGER_BASE=$1~1
-  MERGER_DIFF=$1
+if $plain; then
+  if [[ -z $3 ]]; then
+    echo "[ WW ] Using previous commit as base, MERGER-BASE as MERGER-DIFF" 1>&2
+    MERGER_BASE=$2~1
+    MERGER_DIFF=$2
+  else
+    MERGER_DIFF=$3
+  fi
 else
-  MERGER_DIFF=$2
+  if [[ -z $2 ]]; then
+    echo "[ WW ] Using previous commit as base, MERGER-BASE as MERGER-DIFF" 1>&2
+    MERGER_BASE=$1~1
+    MERGER_DIFF=$1
+  else
+    MERGER_DIFF=$2
+  fi
 fi
 
 git diff-index $MERGER_BASE > /tmp/git-merger-base
 
-git diff-index $MERGER_DIFF |\
-  diff /tmp/git-merger-base - |\
-  awk '/^[<>]/ {printf "%s %s %s\n", $1, $6, $7}' | sort -uk3
+if $plain; then
+  git diff-index $MERGER_DIFF |\
+    diff /tmp/git-merger-base - |\
+    awk '/^[<>]/ {printf "%s %s %s %s\n", $1, $2, $6, $7}' | sort -uk3 |\
+    awk '{printf "%s ", $4}'
+else
+  git diff-index $MERGER_DIFF |\
+    diff /tmp/git-merger-base - |\
+    awk '/^[<>]/ {printf "%s %s %s %s\n", $1, $2, $6, $7}' | sort -uk3
+fi
 
 rm -f /tmp/git-merger-base
