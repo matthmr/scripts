49dd5c295a7a9f7712b6d0018ec15f611d27a82e
mh <observermatth@gmail.com>
2024-06-30 06:50:56 -0300

    up(root/sync-clock.sh): call to `chronyd` directly, much better than sleeping,
      as it only exits when the clock has been updated successfully
    
    up(emacs/emacss): using `pkill` instead of `killpid`
    mv(misc/handler.sh -> misc/uhandle): this names fits better (URI handle)
    ch(sh/functions): `gitat` -> `gat`
    fix(sh/killpid): syntax error typo

--- root/sync-clock.sh
+++ root/sync-clock.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/sh
 
-TIMEOUT=10
-
 if [[ "$USER" != 'root' ]]
 then
   echo "[ !! ] Need to be root"
@@ -10,30 +8,11 @@ fi
 
 case $1 in
   '-h'|'--help')
-    echo "Usage:       sync-clock.sh [-l : latch on]"
+    echo "Usage:       sync-clock.sh"
     echo "Description: Update system clock with \`NTP' and \`chrony'"
-    exit 1
+    exit 1 ;;
 esac
 
 echo "[ .. ] Updating clock"
-rc-service chrony start
 
-if [[ $1 = '-l' ]]
-then
-  echo "[ .. ] Latching on"
-  exit 1
-fi
-
-(
-  {
-    echo "[ .. ] Sleeping $TIMEOUT seconds"
-    sleep $TIMEOUT
-
-    echo "[ .. ] Turning off chrony"
-    rc-service chrony stop
-
-    echo "[ OK ] sync-clock.sh: Done"
-  } &
-)
-
-exit 0
+chronyd -d -q
