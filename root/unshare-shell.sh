4e8e3c6127caa66cb6270ad443588129945e9954
mh <observermatth@gmail.com>
2024-10-07 08:55:47 -0300

    up(root/unshare.sh): the default script for any shell unsharing:
      rm(root/unshare-shell.sh): call `root/unshare.sh` instead
      ad(root/unshare-low-priv.sh): script to run an unshared command with low
        privelege

--- root/unshare-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-if [[ $USER != 'root' ]]; then
-  echo "[ !! ] Need to be root"
-  exit 1
-fi
-
-exec unshare -n -- @UNSHARE_SHELL_UNSHARE@
--- root/unshare.sh
+++ root/unshare.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/sh
 
-echo "[ .. ] Setting LO up"
-sleep 3
-ip link set lo up
+case $1 in
+  '--help'|'-h')
+    echo "Usage:       unshare.sh [COMMAND]"
+    echo "\
+Description: Run COMMAND within an unshared network environment. If
+             empty, spawn a shell instead. Pass LOGINCMD and the shell to
+             execute a preamble command before spawning the shell"
+    exit 0
+    ;;
+esac
 
-echo "[ .. ] SU'ing into unprivileged user"
-exec su @UNSHARE_USER@ --shell=/usr/bin/zsh
+if [[ $USER != 'root' ]]; then
+  echo "[ !! ] Need to be root"
+  exit 1
+fi
+
+exec unshare -n -- @UNSHARE_LOW_PRIV@ $@
