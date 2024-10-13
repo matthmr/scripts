be773a030cd274525900f8164686f1e9df207c6f
mh <observermatth@gmail.com>
2024-10-13 15:06:15 -0300

    up(root/unshare.sh): remove the `low-priv` script, in favor of passing the
      arguments directly into `su`
      - rm(root/unshare-low-priv.sh)
    
    up(x/xdaemon): because this file now takes the foreground in the session stack,
      we call `xrdb` without the ampersand
    up(fzf/fzfc): add `ctrl-space` as a bind (toggle selection)

--- root/unshare.sh
+++ root/unshare.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+SUDO=doas
+
 case $1 in
   '--help'|'-h')
     echo "Usage:       unshare.sh [COMMAND]"
@@ -11,9 +13,14 @@ Description: Run COMMAND within an unshared network environment. If
     ;;
 esac
 
-if [[ $USER != 'root' ]]; then
-  echo "[ !! ] Need to be root"
-  exit 1
+args="$@"
+
+cmd="su $USER"
+
+if [[ -z $args ]]; then
+  cmd+=" -c \"$SHELL; exit 0\""
+else
+  cmd+=" -c \"$args; exit 0\""
 fi
 
-exec unshare -n -- @UNSHARE_LOW_PRIV@ $@
+eval exec $SUDO unshare -n -- $cmd
